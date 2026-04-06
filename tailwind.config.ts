e130b228bbefcf096cf83ef17d055423aafcd968
Felix Fanghaenel <felix.fanghaenel@nitrex.com>
2026-04-06 10:21:23 +0200

    feat: Adapt color to utilize qmulus branding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: colors.blue,
-        secondary: colors.gray,
+        primary: {
+          50: '#fff4f0',
+          100: '#fde8de',
+          200: '#fccbb8',
+          300: '#f7a887',
+          400: '#ef8960',
+          500: '#e97a50',
+          600: '#e5673e',
+          700: '#c4521e',
+          800: '#9e4119',
+          900: '#7a3215',
+          950: '#4e1e0a',
+        },
+        secondary: colors.slate,
 
         provider: {
           apple: '#000000',
